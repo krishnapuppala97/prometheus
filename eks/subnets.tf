c2ed6da0352647a57edd7ce056d47b502421579a
krishnapuppala97 <95561581+krishnapuppala97@users.noreply.github.com>
2023-03-10 15:31:12 +0530

    Update subnets.tf

--- eks/subnets.tf
+++ eks/subnets.tf
@@ -1,9 +1,9 @@
 variable "subnet_id_1" {
   type = string
-  default = "subnet-0bc43***********"
+  default = "subnet-0bc43bf38d079f565"
  }
 
  variable "subnet_id_2" {
   type = string
-  default = "subnet-010796***************"
+  default = "subnet-01079667a8c7ab94f"
  }
